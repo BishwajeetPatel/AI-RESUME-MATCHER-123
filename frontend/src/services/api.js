7789ed9d558af73cccf63ea81900ace0159273c9
BISHWAJEET PATEL <153221241+BishwajeetPatel@users.noreply.github.com>
2025-10-26 14:19:02 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 // src/services/api.js
 import axios from 'axios';
 
+
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://ai-resume-matcher-123-backend32.onrender.com/api';
+
 
 
 const api = axios.create({
